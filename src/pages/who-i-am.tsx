3ed54d30d93c4ea0ae8dd3830d3f9f564f9df4a2
Michael Krog <mic@apaq.dk>
2022-01-24 20:15:32 +0100

    Reworked personal text.

--- src/pages/who-i-am.tsx
+++ src/pages/who-i-am.tsx
@@ -3,7 +3,6 @@ import { Section } from '../components/layout/Section';
 import { Base } from '../templates/Base';
 import { shuffle } from '../utils/shuffle';
 import Tabs from '../components/Tabs';
-import Link from 'next/link';
 
 class WhoIAm extends React.Component {
     imageClasses = 'rounded-full drop-shadow object-cover w-24 h-24 lg:w-27 lg:h-27';
@@ -44,24 +43,23 @@ class WhoIAm extends React.Component {
                             track the implementation of a prototype design all the way to the core.
                         </p>
                         <p className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-                            In 2003 I married Hannah, the <strong>love of my life</strong>. Together we live 
-                            in <a href="https://maps.apple.com/?address=Sjælland, Danmark&ll=55.545598,11.697374&q=Sjælland&_ext=EiYpsJNVbCd7S0AxObMC6Jy8JUA5DUQFPYoQTEBBw9CPK5pHKUBQDA==">Denmark</a> where we have a house and a cat - we named it Thorkild. We have a lot of fun.
-                            Come to think of it - I think we enjoy fun. And food. Both fun and food is important.
+                            Through years of personal experience I have learned how important code quality is. I have 
+                            seen companies come and go, but it is always the ones that focus on delivering easy to use 
+                            products of good quality that prevails in the end. It makes the customers as well as the engineers
+                            want to stay.
                         </p>
                         <p className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-                            I learned from hard experiences that <strong>having fun is important</strong>. Now, I don't 
-                            recognize stress or anxiety as something we should aim at getting rid of - in fact it can be 
-                            a favorable feeling - but it many cases it can 
-                            also become unpleasant when our reaction is based on fear.
-                            It is why I emphasize the need to take care of our mental health and write about it 
-                            in <Link href="/blog">my blog</Link>. I want to help people cope better in a world that can be 
-                            quite stressfull. Lowering our stress level gives us <strong>more joy</strong> and 
-                            makes us more productive.
+                            I find that a both a <strong>strong focus</strong> and a sense of <strong>humor is important</strong>. 
+                            It helps us cope in very stressfull situations, gives us more joy and even makes us more productive. 
+                            And sharing our joy tends to energize people around us. That is why I spend time each day finding out how
+                            I can do it even more.
                         </p>
                         <p className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-                            Being joyful and showing love to others tends to energize people around us. Each day 
-                            I spend time meditating on how <strong>I can improve my personal qualities</strong> in an effort
-                            to improve my relations and pass a bit of the goodness I have received on to others.
+                            From a more personal perspective, in 2003 I married Hannah - the <strong>love of my life</strong>. 
+                            Together we live 
+                            in <a href="https://maps.apple.com/?address=Sjælland, Danmark&ll=55.545598,11.697374&q=Sjælland&_ext=EiYpsJNVbCd7S0AxObMC6Jy8JUA5DUQFPYoQTEBBw9CPK5pHKUBQDA==">Denmark</a> where 
+                            we have a house and a cat - we named it Thorkild. We have a lot of fun.
+                            Come to think of it - I think we enjoy fun. And food. Both fun and food is important. <sl-icon name="emoji-smile"></sl-icon>
                         </p>
                     </div>
                 </Section>
